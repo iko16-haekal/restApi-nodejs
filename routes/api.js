2ff700e69b339ab94cb5dd8565107795fff30b93
iko69 <ikobagas69@gmail.com>
2020-05-02 14:22:03 +0700

    implement middleware to route post

--- routes/api.js
+++ routes/api.js
@@ -2,28 +2,12 @@ var express = require("express");
 var router = express.Router();
 const postController = require("../controllers/postController");
 const cors = require("../middleware/cors");
-/* GET users listing. */
-
-// var cors = require("cors");
-
-// var whitelist = ["http://localhost:8081"];
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
-
-//cors
 
 router.get("/post", cors, postController.findAll);
-router.get("/post/published", postController.findPublished);
-router.get("/post/:id", postController.findOne);
-router.post("/post", postController.create);
-router.put("/post/:id", postController.update);
-router.delete("/post/:id", postController.delete);
-router.delete("/post", postController.deleteAll);
+router.get("/post/published", cors, postController.findPublished);
+router.get("/post/:id", cors, postController.findOne);
+router.post("/post", cors, postController.create);
+router.put("/post/:id", cors, postController.update);
+router.delete("/post/:id", cors, postController.delete);
+router.delete("/post", cors, postController.deleteAll);
 module.exports = router;
